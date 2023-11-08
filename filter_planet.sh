75ea754257020d5e99d94d9aba7fdfa929a341c1
Jacob Pfeifer <jacob@pfeifer.dev>
2023-11-08 17:25:08 -0500

    better planet file filter

--- filter_planet.sh
+++ filter_planet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-osmium tags-filter planet-latest.osm.pbf "nw/highway!=pedestrian,path,cycleway,footway,track" -o filtered.osm.pbf --overwrite
+osmium tags-filter planet-latest.osm.pbf "nw/highway==motorway,trunk,primary,secondary,tertiary,unclassified,residential,motorway_link,trunk_link,primary_link,secondary_link,tertiary_link" -o filtered.osm.pbf --overwrite
